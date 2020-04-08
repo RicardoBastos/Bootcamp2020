4825828bc85e153665cf6bb146ee197f71d1d8ab
Ricardo Bastos <Ricardo.Bastos@genialinvestimentos.com.br>
2020-04-08 17:57:40 -0300

    Node

---
+++ nodejs/src/app.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+
+/**
+ * Middeware
+ */
+function logRequest(request, response, next){
+  const{method, url}= request;
+  const log = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(log);
+
+  return next();
+}
+
+app.use(logRequest);
+
+
+
+app.get("/repositories", (request, response) => {
+  response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const{title,url,techs}= request.body;
+
+  const likes = 0;
+  const repository = {id:uuid(), title, url, techs, likes};
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const{id}= request.params;
+  const{title, url, techs, likes} = request.body;
+
+  const repositoryIndex = repositories.findIndex(respo=>respo.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({"error":"Repository not found"});
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[repositoryIndex]=repository;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const{id}= request.params;
+
+  const repositoryIndex = repositories.findIndex(respo=>respo.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({"error":"Repository not found"});
+  }
+
+  repositories.splice(repositoryIndex,1);
+
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const{id}= request.params;
+
+  const repositoryIndex = repositories.findIndex(respo=>respo.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({"error":"Repository not found"});
+  }
+
+  const repository = repositories.find(respo=>respo.id === id);
+
+  const repositoryUpdate = {...repository, likes: repository.likes + 1};
+
+  repositories[repositoryIndex]=repositoryUpdate;
+
+  return response.json(repositoryUpdate);
+
+});
+
+module.exports = app;
